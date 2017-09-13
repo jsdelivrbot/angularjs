d96041632540f6e63f5c0b3cf581a4e59a6b2776
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2017-09-13 18:47:50 -0500

    Initial commit

---
+++ angularjsnodejs/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var serverStatic = require('server-static');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+
+//Conexion a la base de datos
+mongoose.connect('mongoose://localhost:27017/angularjsnodejs');
+
+//Configuracion
+
+//Localizacion de ficheros estaticos
+app.use(serverStatic('/public', {'index': ['index.html', 'index.htm']}));
+
+//Muestra un log de todos los request
+app.use(morgan('dev'));
+
+//Permite cambiar el HTML por el metodo POST
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Simula el DELETE and PUT
+app.use(methodOverride());
+
+app.listen('8081', () => {
+    console.log('La aplicacion esta escuchando por el puerto 8081');
+});
