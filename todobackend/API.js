100ffd74185e9aa0b4a5ad0a1af2c4200fe799da
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2017-09-15 12:30:01 -0500

    Se agregan los cambios createdAt y updatedAt

--- todobackend/API.js
+++ todobackend/API.js
@@ -30,7 +30,10 @@ router.post('/create', (req, res) => {
         res.status(500).send(errors);
     }
 
+    req.body.createdAt = new Date();
+    req.body.updatedAt = new Date();
     tasks.push(req.body);
+    
     res.status(200).json(req.body);
 });
 
