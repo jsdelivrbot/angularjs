28ab75b97adb9cafbefde76e8a457869ea73fac5
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2017-09-15 10:56:50 -0500

    Project test Rok33r labs tasks

--- todobackend/index.js
+++ todobackend/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { check, oneOf, validationResult } = require('express-validator/check');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,14 +11,36 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.post('/task/create', (req, res) => {
-  debugger;
+var tasks = [];
+
+app.post('/task/create', [
+  check('name').exists(),
+  check('dueDate').exists(),
+  check('priority').exists().isInt(),
+  
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(500).send({
+      errors: errors.mapped()
+    })
+  }
+  
+  tasks.push(req.body);
 });
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
+
