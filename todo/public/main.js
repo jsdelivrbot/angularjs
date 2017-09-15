85607dd59ba12cf13f08c55bd1d4ec64396ee341
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2017-09-15 11:40:47 -0500

    Validaciones de las tareas para el registro

--- todo/public/main.js
+++ todo/public/main.js
@@ -1,33 +1,38 @@
 angular.module('angularjsnodejs', []).controller('mainController', mainController);
 
 function mainController($scope, $http) {
-    $scope.formData = {};
-    $scope.tasks = [];
+    var vm = this;
+
+    vm.formData = {};
+    vm.tasks = [];
+
+    vm.currentDate = new Date();
+    vm.currentDate.setHours(0, 0, 0, 0);
 
     $http.get('https://glacial-savannah-46887.herokuapp.com/task')
         .then(function (data) {
-            $scope.tasks = data.data;
+            vm.tasks = data.data;
             console.log(data)
         }, function (data) {
             console.error('Error: ', data);
         });
 
-    $scope.createTask = function () {
-        $scope.formData.id = $scope.tasks.length + 1;
-        $http.post('https://glacial-savannah-46887.herokuapp.com/task/create', $scope.formData)
+    vm.createTask = function () {
+        vm.formData.id = vm.tasks.length + 1;
+        $http.post('https://glacial-savannah-46887.herokuapp.com/task/create', vm.formData)
             .then((data) => {
-                $scope.formData = {};
-                $scope.tasks.push(data.data);
+                vm.formData = {};
+                vm.tasks.push(data.data);
                 console.log(data);
             }, (data) => {
                 console.log(`Error`, data);
             });
     };
 
-    $scope.deleteTask = function (id) {
+    vm.deleteTask = function (id) {
         $http.get(`https://glacial-savannah-46887.herokuapp.com/task/destroy/${id}`)
             .then((data) => {
-                $scope.tasks = $scope.tasks.filter((task) => {
+                vm.tasks = vm.tasks.filter((task) => {
                     return task.id != id;
                 });
                 console.log(data);
@@ -36,15 +41,11 @@ function mainController($scope, $http) {
             });
     };
 
-    $scope.taskPendding = function (task) {
-        var currentDate = new Date();
-        currentDate.setHours(0,0,0,0);
-        return new Date(task.dueDate) >= currentDate;
+    vm.taskPendding = function (task) {
+        return new Date(task.dueDate) >= vm.currentDate;
     }
 
-    $scope.taskOverdue = function (task) {
-        var currentDate = new Date();
-        currentDate.setHours(0,0,0,0);
-        return new Date(task.dueDate) < currentDate;
+    vm.taskOverdue = function (task) {
+        return new Date(task.dueDate) < vm.currentDate;
     }
 }
