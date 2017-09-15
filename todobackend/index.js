f55d4b638b070064d1c5950711b09e58f9d78ccc
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2017-09-15 12:00:13 -0500

    Todo Backend

--- todobackend/index.js
+++ todobackend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const { check, oneOf, validationResult } = require('express-validator/check');
+
+var tasks = [];
+
+app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,7 +14,6 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -22,25 +25,55 @@ app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-var tasks = [];
+app.post('/task/create', (req, res) => {
 
-app.post('/task/create', [
-  check('name').exists(),
-  check('dueDate').exists(),
-  check('priority').exists().isInt(),
-  
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(500).send({
-      errors: errors.mapped()
+  var errors = [{
+    ValidationError: {
+      name: []
+    }
+  }];
+
+  if (!req.body.name) {
+    errors[0].ValidationError.name.push({
+      data: null,
+      message: "Validation error: \"null\" Rule \"required(true)\" failed.",
+      rule: "required",
+      args: [
+        true
+      ]
     })
   }
-  
+
+  if(errors[0].ValidationError.name.length > 0){
+    res.status(500).send(errors);
+  }
+
   tasks.push(req.body);
+  res.status(200).json(req.body);
+});
+
+app.get('/task', (req, res) => {
+  res.json(tasks);
+});
+
+app.get('/task/destroy/:id', (req, res) => {
+  var index = tasks.findIndex((item) => {
+    return item.id == req.params.id
+  });
+
+  if (index == -1) {
+    res.status(400).send('Task not found');
+  }
+
+  tasks.splice(index, 1);
+
+  res.status(200).send('Task destroy');
+});
+
+app.post('/task/update', (req, res) => {
+
 });
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
-
